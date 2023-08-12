91b88fc9a1d958c64e2aa547017f7bb2e39b9905
AntonioMesquit <140577539+AntonioMesquit@users.noreply.github.com>
2023-08-12 16:37:39 -0300

    up

---
+++ estruturas/javscript.js
@@ -0,0 +1,18 @@
+let nome = "Mateus"
+console.log(nome)
+console.log(`Mateus`)
+
+nome = "Antonio Mesquita"
+
+const idade = 17
+
+console.log(idade)
+
+/*
+const age = prompt("Digite sua idade :");
+console.log (`Voce tem ${age} anos`)
+*/
+
+console.log(Math.max(10,2,4,6,2,1,2,4,6))
+console.log(Math.floor(5.41))
+
