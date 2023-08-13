b7ff1da87de6e569235b89a09781097964efcecd
AntonioMesquit <140577539+AntonioMesquit@users.noreply.github.com>
2023-08-12 23:07:54 -0300

    Update javascript.js

--- Funcoes/javascript.js
+++ Funcoes/javascript.js
@@ -63,9 +63,9 @@ escopoalinhado();
 const textarrow = () => {
     console.log("Esta e uma arrow function")
 }
-textarrow()*/
+textarrow()
 
-const parouimpar = (n) => {
+const parouimpar = (n) => { 
     if(n % 2 === 0){
         console.log("Par")
     }
@@ -77,3 +77,145 @@ const parouimpar = (n) => {
 const m = 11;
 
 parouimpar(m)
+
+const raizquadrada = (x) => {
+    return x * x
+
+}
+console.log(raizquadrada(4))
+
+const raizquadrada2 = (x) => x * x
+
+console.log(raizquadrada2(5))
+console.log(raizquadrada2(12))
+
+
+
+const mutiplicacao = function (m,n) {
+    if (n === undefined){
+        return m * 2;
+    }
+    else {
+       return m * n
+    }
+}
+
+const name = "marcos"
+const greeting = (name) => {
+    if (!name) {
+        console.log("ola")
+    }
+    console.log(`ola ${name}`)
+}
+
+console.log(greeting())
+console.log(greeting("mateus"))
+
+
+
+const nombre = (nombre , name = "ola") => {
+    return `${nombre}, ${name}! ` 
+}
+
+console.log(nombre("mateus"))
+ 
+
+
+const repetir = (text, repito = 2) => {
+    for(let i = 0; i < repito; i++){
+        console.log(text)
+    }
+}
+repetir("vai tomar no cu" , 5 )
+
+
+function some() {
+    let txt = "Alguma coisa"
+
+    function display (){
+        console.log(txt)
+    }
+    display()
+}
+some()
+
+
+
+const mutipl = (n) => {
+    return (m) => {
+        return n * m;
+    }
+}
+const c1 = mutipl(5)
+const c2 = mutipl(10)
+console.log(c1(5))
+
+
+const atedez = (n , m) => {
+    if (n < 10) {
+        console.log("A funcao parou de executar!")
+    }
+    else {
+        const x = n - m;
+        console.log(x)
+        return atedez(x, m)
+    }
+    
+    
+}
+*/
+
+function fatorial (x){
+    if (x === 0){
+        return 1 
+    }
+    else { 
+        return x * fatorial(x - 1)
+    }
+}
+console.log(fatorial())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
