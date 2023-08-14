09a7906477c98f9e52a50bdbd398aef2033a7126
AntonioMesquit <140577539+AntonioMesquit@users.noreply.github.com>
2023-08-13 22:24:43 -0300

    UP

---
+++ arrays/java.js
@@ -0,0 +1,174 @@
+/*
+const lista = [1,2,3,4,5]
+
+const itens = ["mateus " , 1 , 2.4 , true]
+
+console.log(itens)
+
+
+
+
+console.log(nome.length)
+
+
+
+const ar = ["a" , "b", "c" ,"a" ]
+console.log(ar[])
+
+
+
+const numos = [1,2,3]
+const nu = [1,2,3]
+const num = numos.concat(nu)
+
+console.log(num)
+
+const text = "antonio"
+
+console.log(text.toUpperCase()
+)
+console.log(text.indexOf("n"))
+
+
+
+const pessoa = {
+    name: "Mateus",
+    age: 31,
+    job: "Programador",
+}
+
+console.log(pessoa)
+
+console.log(pessoa.job)
+
+
+const car = {
+    motor: 2.0,
+    marca: "VW",
+    mod: "Tiguan",
+    km: 20000,
+}
+
+car.porta = 4;
+
+console.log(car)
+
+delete car.km
+
+console.log(car)
+
+const katia = {
+    prof: "prostituta",
+    idade: 72,
+    valor: 0.50,
+}
+console.log(katia)
+
+del katia.valor
+
+const obj = {
+    a: "teste",
+    b: true,
+}
+console.log(obj instanceof Object)
+
+const obj2 = {
+    c : []
+}
+Object.assign(obj2, obj)
+
+console.log(obj2)
+delete obj2.c
+console.log(obj2)
+
+console.log(Object.keys(obj2))
+
+console.log(Object.entries(obj))
+
+*/
+const a = {
+    name: "antonio"
+}
+const b = a;
+console.log(b)
+console.log(a)
+
+a.age = 31
+
+console.log(b)
+
+console.log(a === b)
+
+delete b.age 
+
+console.log(b)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
