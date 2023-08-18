24ecfbeca132cd2ac44ad93a9fe93e5d75ce2782
Antonio Jose Mesquita Silveira <140577539+AntonioMesquit@users.noreply.github.com>
2023-08-18 10:29:44 -0300

    up

--- arrays/node_6a3f9c7b3f339.tmp.js
+++ arrays/node_6a3f9c7b3f339.tmp.js
@@ -138,130 +138,17 @@ const itemremorivo = array.pop()
 console.log(itemremorivo)
 .array.push("z" , "x")
 
+*/
 
 
 
 const reverse = [1,2,3,4,5]
 
 reverse.reverse()
-console.log(reverse)
-
-
-
-TRIM
-
-
-const trim = "   testando \n"
-
-console.log(trim)
-console.log(trim.trim())
-
-
-padStart e padEnd
-
-
-const testepad = "1"
-
-const newT = testepad.padStart( 4,"4")
-const newS = newT.padEnd(10 ,"0")
-
-console.log(testepad)
-console.log(newT)
-console.log(newS)
-
-SPLIT
-
-const frase = "o rato roeu a roupa da karla"
-const array = frase.split(" ");
-
-console.log(array)
-
-
-JOIN
 
 
 
 
-const peri = ["Mouse", "Teclado", "MousePad"]
-const k = `Precisamos comprar ${peri.join(",  ")}.`
-console.log(k)
-
-REPEAT
-
-
-const palavra = "Testando ";
-console.log(palavra.repeat(5))
-
-
-Rest Operator / Rest Parameters
-
-
-const soma = (...args) => {
-     let total = 0
-
-     for( i = 0; i < args.length; i++){
-        total+= args[i]
-     }
-     return total
-}
-
-console.log(soma(1,2,3))
-
-Estrutura repeticao for of
-
-
-const soma = (...args) => {
-    let total = 0
-
-    for(num of args){
-        total += number 
-    }
-    return total
-}
-console.log(soma(1,2,3))
-
-
-
-destructing em objestos
-
-
-const user = {
-    nome1: "Mateus",
-    nome2: "Battisti",
-    job: "Programador",
-}
-const {nome1,nome2,job} = user;
-console.log(nome1,nome2,job)
-
-// renomear
-const{nome1: first} = user;
-
-console.log(first)
-
-
-destruct com arrays
-
-const list = ["Aviao" , "Carro" , "Submarino"]
-const [veiculoA, veiculoB, veiculoC] = list;
-
-console.log(veiculoA,veiculoB,veiculoC)
-*/
-// JSON
-
-const json2 = '{"name": "Mateus", "age": 31, "skills":["PHP" , "JavaScript" , "Python" ]}'
-const obj = JSON.parse(json2)
-console.log(obj)
-
-console.log(obj.name)
-
-// jason invalido
-
-const badJson = '{"name": Matheus, "age": 31'
-
-console.log(obj)
-const newJson = JSON.stringify(obj)
-
-console.log(newJson)
 
 
 
