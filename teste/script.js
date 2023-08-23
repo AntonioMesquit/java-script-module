5725473009f0d764868465e40cdd5f8fd2bef49a
Antonio Jose Mesquita Silveira <140577539+AntonioMesquit@users.noreply.github.com>
2023-08-23 11:40:19 -0300

    up

--- teste/script.js
+++ teste/script.js
@@ -1,7 +1,13 @@
-const varial = document.getElementsByClassName('box');
+const func = document.getElementsByClassName('box')
 
-for(i = 0; i < varial.length; i++){
-    varial[i].addEventListener('click' , function(){
-        this.classList.toggle('ativo')
-    })
+for(i = 0; i < func.length; i++){
+    func[i].addEventListener('click' , function(){
+        const ativado = this.classList.contains('ativo');
+        for(let j = 0; j < func.length; j++){
+            func[j].classList.remove('ativo');
+        }
+        if(!ativado){
+            this.classList.add('ativo')
+        }
+    });
 }
