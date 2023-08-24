8f7bd2cf3061131cf3362f24fbdcd52196e1b511
AntonioMesquit <140577539+AntonioMesquit@users.noreply.github.com>
2023-08-24 14:22:08 -0300

    up

--- teste/script.js
+++ teste/script.js
@@ -1,7 +1,1 @@
-const varial = document.getElementsByClassName('box');
-
-for(i = 0; i < varial.length; i++){
-    varial[i].addEventListener('click' , function(){
-        this.classList.toggle('ativo')
-    })
-}
+console.log("oi")
