e2bcb6ecfced2c771f0bc280f8afa20a6a82dce6
AntonioMesquit <140577539+AntonioMesquit@users.noreply.github.com>
2023-08-28 20:52:38 -0300

    up

--- teste/script.js
+++ teste/script.js
@@ -1,1 +1,15 @@
-console.log("oi")
+const click = document.querySelectorAll(".x");
+
+
+for (let i = 0; i < click.length; i++) {
+    click[i].addEventListener('click' , function() {
+        const active = this.classList.contains("active")
+   
+    for(let j = 0; j < click.length; j++){
+        click[j].classList.remove("active")
+    }
+    if(!active){
+        this.classList.add("active")
+    } 
+})
+}   
